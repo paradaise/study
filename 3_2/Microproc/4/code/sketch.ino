0df07bd414af08372ad32491c1c3f3be1c3c86ba
paradaise <paradaiseru@gmail.com>
2025-03-20 22:05:54 +0300

    change

--- 3_2/Microproc/4/code/sketch.ino
+++ 3_2/Microproc/4/code/sketch.ino
@@ -1,31 +1,28 @@
-boolean buttonWasUp = true;
-boolean ledEnabled = false;
+boolean buttonWasUp = true; // предполагаем что опущена в передыщем цикле
+boolean ledEnabled = false; // и светодиод не горит
 
 void setup() {
-  pinMode(13, OUTPUT);
-  pinMode(2, INPUT_PULLUP);
+  pinMode(13, OUTPUT); //управление светодиодом на выход
+  pinMode(2, INPUT_PULLUP); //управление кнопкой на вход
 }
  
 void loop() {
-   // узнаем, отпущена ли кнопка сейчас
+
    boolean buttonIsUp = digitalRead(2);
  
-   // если кнопка была отпущена и не отпущена сейчас
+
    if (buttonWasUp && !buttonIsUp) {
 
-      // исключаем дребезг контактов тактовой кнопки
+
       delay(10);
 
-    // и считываем сигнал с кнопки снова
     buttonIsUp = digitalRead(2);
 
-      // если кнопка нажата, то переворачиваем сигнал светодиода
       if (!buttonIsUp) {
          ledEnabled = !ledEnabled;
          digitalWrite(13, ledEnabled);
       }
    }
  
-   // запоминаем состояние кнопки для новой итерации
    buttonWasUp = buttonIsUp;
 }
